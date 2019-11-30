0e8a172178dc40896c51541a3bb6ae88e57ec00c
wse_vikaspulluri <vikas.pulluri@wallstreetenglish.com>
2019-11-30 21:57:25 +0530

    clustering

--- main/index.js
+++ main/index.js
@@ -1,11 +1,14 @@
+process.env.UV_THREADPOOL_SIZE = 1;
+
 const cluster = require('cluster');
+const crypto = require('crypto');
 
 // console.log(cluster.isMaster);
 // Is the file being executed in master mode?
 if (cluster.isMaster) {
   // Cause index.js to be executed *again* but in slave/child mode
   cluster.fork();
-  cluster.fork();
+  // cluster.fork();
   // cluster.fork();
   // cluster.fork();
 } else {
@@ -13,14 +16,11 @@ if (cluster.isMaster) {
   const express = require('express');
   const app = express();
 
-  function doWork(duration) {
-    const start = Date.now();
-    while(Date.now() - start < duration) {}
-  }
-
   app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hello there!!!');
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      res.send('Hello there!!!');
+    });
+
   })
 
   app.get('/fast', (req, res) => {

7b2a24c797b0f5119a938c4dc3408a158e453e87
wse_vikaspulluri <vikas.pulluri@wallstreetenglish.com>
2019-11-30 22:09:38 +0530

    pm2 usage

--- main/index.js
+++ main/index.js
@@ -1,33 +1,23 @@
-process.env.UV_THREADPOOL_SIZE = 1;
-
-const cluster = require('cluster');
 const crypto = require('crypto');
 
-// console.log(cluster.isMaster);
-// Is the file being executed in master mode?
-if (cluster.isMaster) {
-  // Cause index.js to be executed *again* but in slave/child mode
-  cluster.fork();
-  // cluster.fork();
-  // cluster.fork();
-  // cluster.fork();
-} else {
-  // I'm a child, iam going to act like a server, nothing else
-  const express = require('express');
-  const app = express();
+// I'm a child, iam going to act like a server, nothing else
+const express = require('express');
+const app = express();
 
-  app.get('/', (req, res) => {
-    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-      res.send('Hello there!!!');
-    });
+app.get('/', (req, res) => {
+  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    res.send('Hello there!!!');
+  });
 
-  })
+})
 
-  app.get('/fast', (req, res) => {
-    res.send('this is fast');
-  })
+app.get('/fast', (req, res) => {
+  res.send('this is fast');
+})
 
-  app.listen(3000, () => console.log('server listening on 3000'));
-}
+app.listen(3000, () => console.log('server listening on 3000'));
 // server benchmarks commands
 // ab -c 50 -n 500 localhost:3000/fast
+// pm2 start index.js -i 0
+// the -i 0 above tells the pm2 to create instances based on the logical cores availability
+// no. of logical cores = no. of physical cores * no. of processes a core runs at a given time

5af1ed5f9a36192e60e14167a0713ab735fc8934
wse_vikaspulluri <vikas.pulluri@wallstreetenglish.com>
2019-12-17 22:09:56 +0530

    Cache implementation

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,6 +3,12 @@ const requireLogin = require('../middlewares/requireLogin');
 
 const Blog = mongoose.model('Blog');
 
+const redis = require('redis');
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+const util = require('util');
+client.get = util.promisify(client.get); // returns promise from callback fns
+
 module.exports = app => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
@@ -14,9 +20,11 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
 
+    const blogs = await Blog.find({ _user: req.user.id }).cache();
+    console.log('SEREVING FROM MONGODB');
     res.send(blogs);
+
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
---
+++ services/cache.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.cache = function() {
+  this.useCache = true;
+  return this;
+}
+
+mongoose.Query.prototype.exec = async function() {
+  // console.log(this.getQuery());
+  // console.log(this.mongooseCollection.name);
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+  const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name
+  }));
+
+  const cacheValue = await client.get(key);
+  if (cacheValue) {
+    // console.log(cacheValue); // Stringified JSON object
+    const doc = JSON.parse(cacheValue);
+    // return JSON.parse(cacheValue); // won't work as exec should return model instance
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc);
+  }
+
+  const result = await exec.apply(this, arguments);
+  // result is not a plain js object. it is mongoose document(model instance)
+  // you can verify it from result.validate call
+
+  client.set(key, JSON.stringify(result));
+
+  return result; // You have to return what actual mongoose exec will returns
+}

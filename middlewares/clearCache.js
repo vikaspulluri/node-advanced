a567d219ecfbed52357e486e47ec1445c3a25f2e
wse_vikaspulluri <vikas.pulluri@wallstreetenglish.com>
2020-02-24 22:02:26 +0530

    deleting unnecessary files

--- middlewares/clearCache.js
+++
@@ -1,7 +0,0 @@
-const { clearHash } = require('../services/cache');
-
-module.exports = async (req, res, next) => {
-  await next(); // wait for req handler to finish
-
-  clearHash(req.user.id);
-}
